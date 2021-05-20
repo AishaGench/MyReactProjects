3f5d0b8bc280f064032cd0a6bd623ce5bfbc992c
Aisha Gench <aisha.gench@gmail.com>
2021-05-20 00:04:50 -0500

    export reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 const reducer = (state =0, action)=>{
+
     switch(action.type){
         case "INCREMENT" : return state + 1;
         case "DECREMENT" : return state - 1;
         default: return state;
     }
 }
+export default reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import {createStore} from "reduc";
+import {createStore} from "redux";
 import reducer from "../reducers/index"
 
 const myStore = createStore(reducer)
