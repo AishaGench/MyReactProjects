e42131f4e58922d4529ec70564c8508d13112077
Aisha Gench <aisha.gench@gmail.com>
2021-05-19 23:27:10 -0500

    create reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+const reducer = (state =0, action)=>{
+    switch(action.type){
+        case "INCREMENT" : return state + 1;
+        case "DECREMENT" : return state - 1;
+        default: return state;
+    }
+}
