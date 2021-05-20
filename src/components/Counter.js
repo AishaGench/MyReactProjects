2e99a7f6285b23798d1dbe3ce074f52c81b51dcc
Aisha Gench <aisha.gench@gmail.com>
2021-05-19 23:55:44 -0500

    create another buttuns

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,31 @@ import React, {useState} from 'react'
 
 export default function Counter(props){
     const [state, setState] = useState(false)
+
+    const oddIncrease = () => {
+        if(props.value%2 !== 0){
+            props.onIncrement()
+        }
+    }
+
+    const timeDelay = () => {
+        setTimeout( ()=>{
+            props.onIncrement()
+        }, 1000)
+    }
+    let myTimer;
+
+    const timer = () => {
+
+        if(!state){
+            myTimer = setInterval( ()=> {
+                props.onIncrement()
+            }, 1000)
+        } else {
+            clearInterval(myTimer);
+            
+        }
+
 }
 
 return(
@@ -10,6 +35,9 @@ return(
         <button onClick ={props.onIncrement}>Increment</button>
         <button onClick ={props.onDecrement}>Decrement</button>
         <br/>
+        <button onClick={oddIncrease}>ODD</button>
+        <button onClick={timeDelay}>Time Delay</button>
+        <button onClick={handleClick}>Timer</button>
 
 
     </div>
