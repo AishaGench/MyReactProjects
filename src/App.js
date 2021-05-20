dde8043ba4a6811e0af259147d65647d5c458094
Aisha Gench <aisha.gench@gmail.com>
2021-05-19 23:40:47 -0500

    connect to store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {connect} from 'react-redux'
+import Counter from './components/Counter'
 
 
 function App() {
   return (
     <>
+    <Counter/>
     </>
     )
 }
+const mapStateToProps = (state)=>{
+  return {value:state}
+}
+const mapDispatchToProps = dispatch =>{
+  return{
+    onIncrement: () => dispatch({type:"INCREMENT"}),
+    onDecrement: () => dispatch({type:"DECREMENT"})
+  }
+}
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
